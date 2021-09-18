3a85aad2b7425c1ffd17bcb76e3f6c95734be56c
stef <paulkelele@gmail.com>
2021-09-18 09:52:48 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+const config = {
+  apiKey: "AIzaSyA31NrloaJekNk8IwYagPSwqapJL89Q1aM",
+  authDomain: "project1972-eeb92.firebaseapp.com",
+  databaseURL: "https://project1972-eeb92.firebaseio.com",
+  projectId: "project1972-eeb92",
+  storageBucket: "project1972-eeb92.appspot.com",
+  messagingSenderId: "256631172200",
+  appId: "1:256631172200:web:41e70d58820480c54c1fdd"
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(config)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
