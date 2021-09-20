36adcdb4a20288341272d735c3bdc2578b1ea5ec
stef <paulkelele@gmail.com>
2021-09-20 09:42:29 +0200

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+interface users {
+  name: string
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'WeWeSoft';
+  item$: Observable<users[]>;
+
+  constructor(firestore: Firestore) {
+    const collections: any = collection(firestore, 'users');
+    this.item$ = collectionData(collections);
+  }
 }
